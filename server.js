df66abeef2aad3408ebecb2154212eeed4d21ad4
Pavan Sathwik Gandrakota <sathwikgandrakota145@gmail.com>
2026-01-07 16:41:28 +0530

    Implement CORS and environment variable support
    Added CORS support and dotenv configuration.

--- server.js
+++ server.js
@@ -1,19 +1,34 @@
+const cors = require("cors");
+
 const express = require("express");
+const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 
 const authRoutes = require("./routes/authRoutes");
 const trainRoutes = require("./routes/trainRoutes");
 const bookingRoutes = require("./routes/bookingRoutes");
 
+dotenv.config(); 
+
 connectDB();
 
 const app = express();
+app.use(cors({
+  origin: "http://localhost:5173", 
+}));
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/trains", trainRoutes);
 app.use("/api/bookings", bookingRoutes);
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send("Backend is running successfully 🚀");
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
